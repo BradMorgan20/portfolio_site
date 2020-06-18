2b8d9e1a1548b43e627d13c72728c39e7260978e
Bradderz96 <bradley@colinmorgan.com>
2020-06-18 10:58:04 +0000

    gitignore file update and example settings

--- test_app/settings-example.py
+++ test_app/settings-example.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'r_bulc4-l$*47v+!l&z9s-nag3noel^o8#0^53vfn95x0x_h_r'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.68']
+ALLOWED_HOSTS = ['']
 
 
 # Application definition
